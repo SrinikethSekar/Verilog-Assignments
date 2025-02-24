5b78b64bb5876d50cdfbc9b914172be7be4b4ae0
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-02-24 23:07:32 +0530

    Create 2to4_decoder.v

---
+++ Assignment 1/2to4_decoder.v
@@ -0,0 +1,9 @@
+module decoder2to4(input [1:0] a, output reg [3:0] y);
+  always @(*)
+    case (a)
+      2'b00: y = 4'b0001;
+      2'b01: y = 4'b0010;
+      2'b10: y = 4'b0100;
+      2'b11: y = 4'b1000;
+    endcase
+endmodule
