30419c33d8b00214934d20e350533558dc787ccc
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-02-28 22:17:03 +0530

    Create decoder.v

---
+++ Assignment 3/decoder.v
@@ -0,0 +1,20 @@
+module Decoder(
+  input [1:0]din,
+  output reg [3:0] dout
+);
+  
+  always @(*) begin
+    
+    case(din)
+      
+      2'b00 : dout = 4'b0001 ;
+      2'b01 : dout = 4'b0010 ;
+      2'b10 : dout = 4'b0100 ;
+      2'b11 : dout = 4'b1000 ;
+      
+      
+    endcase
+    
+  end
+  
+endmodule
