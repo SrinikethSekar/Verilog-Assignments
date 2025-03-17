9e6478e612f78b9bc72ec75833c6baae03ac6cd6
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-03-17 22:09:05 +0530

    Create Q7.v

---
+++ Assignment 4/Q7.v
@@ -0,0 +1,20 @@
+//Write a Verilog function to shift the input data 1 bit to the right.
+module Q7;
+
+
+	function [7:0] right_shift;
+		input[7:0] data;
+		begin
+			right_shift=data>>1;
+		end
+		
+	endfunction 
+
+
+	initial begin
+
+	$display("Right shift of 8:%0d",right_shift(8));
+
+	end 
+
+endmodule
